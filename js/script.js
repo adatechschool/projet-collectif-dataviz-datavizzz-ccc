eca413389c0c8c602cfbb43d1ecb2ca014535459
cindylecat <cindylecat5@gmail.com>
2024-03-19 16:46:23 +0100

    integration de texte button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
+const ulElement = document.querySelector("ul");
 const systemeAPI = fetch("https://api.le-systeme-solaire.net/rest/bodies/");
 console.log("systemeAPI = " +systemeAPI);
 systemeAPI.then(response => {
     return response.json();
 }).then(json => {
-    console.log(json.bodies[0].name)
+    //console.log(json.bodies[0].name)
+for (n=0; n<json.bodies.length;n++){
+    if (json.bodies[n].isPlanet === true){
+        const liElement = document.createElement("li");
+        liElement.classList.add("visible");
+        liElement.innerText = json.bodies[n].name;
+        ulElement.appendChild(liElement);
+        const buttonPlanet = document.createElement("button"); 
+        buttonPlanet.innerText = json.bodies[n].name;      
+
+    }
+}
+
+   
+
 })
 
+// liElement.classList.remove("invisible");
+  //  liElement.classList.add("visible");
+
+//});
+//buttonTerre.addEventListener("click",()=>{
+  //  liElement.classList.remove("invisible");
+    //liElement.classList.add("visible");
+
+//});
 function setup(){
     createCanvas(600, 600, WEBGL);
 }
 
+
 function draw(){
-    background(255);
+    background(0,0,0,0);
     //sphere(100);
     normalMaterial();
   stroke(0);
