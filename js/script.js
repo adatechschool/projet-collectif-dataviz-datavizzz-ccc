3b08f0c1e9a005af1762ea6ef61bb79643eee728
cetrl <celiatruel@gmail.com>
2024-03-20 16:47:58 +0100

    planètes stylées

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const ulElement = document.querySelector("ul");
 const systemeAPI = fetch("https://api.le-systeme-solaire.net/rest/bodies/");
+
 console.log("systemeAPI = " +systemeAPI);
+
 systemeAPI.then(response => {
     return response.json();
 }).then(json => {
@@ -13,44 +15,62 @@ for (n=0; n<json.bodies.length;n++){
         ulElement.appendChild(liElement);
         const buttonPlanet = document.createElement("button"); 
         buttonPlanet.innerText = json.bodies[n].name;      
-
     }
 }
 
    
-
 })
+// buttonTerre.addEventListener("click",()=>{
+//  liElement.classList.remove("invisible");
+//     liElement.classList.add("visible");
 
-// liElement.classList.remove("invisible");
-  //  liElement.classList.add("visible");
+// });
+// buttonTerre.addEventListener("click",()=>{
+//     liElement.classList.remove("invisible");
+//     liElement.classList.add("visible");
 
-//});
-//buttonTerre.addEventListener("click",()=>{
-  //  liElement.classList.remove("invisible");
-    //liElement.classList.add("visible");
+// });
 
-//});
 function setup(){
-    createCanvas(600, 600, WEBGL);
+    createCanvas(windowWidth, windowHeight, WEBGL);
 }
-
 
 function draw(){
     background(0,0,0,0);
-    //sphere(100);
-    normalMaterial();
-  stroke(0);
-  
-  push();
-  rotateY(-millis()/1000);
-  sphere(80);
-  pop();
-  
-  push();
-  rotateY(-millis()/1000);
-  translate(200,0);
-  
-  fill(80,10,60);
-  sphere(40);
-  pop();
+    ambientLight(5, 5, 5); // white light
+    pointLight(250, 250, 250, 0, 0, 200);
+    noStroke();
+    
+    fill(255);
+    angleMode(DEGREES);
+    
+    rotateX(-20);
+    
+    push();
+    emissiveMaterial(255, 255, 100);
+    rotateY(-millis()/30);
+    sphere(80);
+    pop();
+    
+
+    push();
+    specularMaterial(0, 0, 255, 10);
+    shininess(0);
+    rotateY(-millis()/30);
+    translate(400,0);
+    sphere(40);
+    pop();
+
+    push();
+    specularMaterial(255, 50, 0, 10);
+    shininess(0);
+    rotateY(-millis()/15);
+    translate(200,0);
+    sphere(15);
+    pop();
 }
+
+// ajuste la taille du sketch en fonction de la taille de la fenêtre
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
