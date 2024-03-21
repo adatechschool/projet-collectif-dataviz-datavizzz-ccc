dcbc0fa2e96eaba69395c6f5e37615942441ba2c
cindylecat <cindylecat5@gmail.com>
2024-03-21 12:39:28 +0100

    avancement du projet

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const ulElement = document.querySelector("ul");
 const systemeAPI = fetch("https://api.le-systeme-solaire.net/rest/bodies/");
-
-console.log("systemeAPI = " +systemeAPI);
+let equaRadius = [];
+let aphelion = [];
 
 systemeAPI.then(response => {
     return response.json();
 }).then(json => {
-    //console.log(json.bodies[0].name)
+    console.log(json.bodies);
 for (n=0; n<json.bodies.length;n++){
     if (json.bodies[n].isPlanet === true){
+        equaRadius.push(json.bodies[n].equaRadius);
+        aphelion.push(json.bodies[n].aphelion);
+
+
+
         const liElement = document.createElement("li");
         liElement.classList.add("visible");
         liElement.innerText = json.bodies[n].name;
@@ -33,6 +38,11 @@ for (n=0; n<json.bodies.length;n++){
 
 function setup(){
     createCanvas(windowWidth, windowHeight, WEBGL);
+    //console.log(equaRadius);
+    zoom = createSlider(0,700,0,1);
+    zoom.position(windowWidth-300,100);
+    zoom.size(200);
+
 }
 
 function draw(){
@@ -40,12 +50,16 @@ function draw(){
     ambientLight(5, 5, 5); // white light
     pointLight(250, 250, 250, 0, 0, 200);
     noStroke();
+
+
     
     fill(255);
     angleMode(DEGREES);
     
-    rotateX(-20);
     
+    translate(0,0,zoom.value());
+    rotateX(-20);
+
     push();
     emissiveMaterial(255, 255, 100);
     rotateY(-millis()/30);
@@ -53,22 +67,39 @@ function draw(){
     pop();
     
 
-    push();
-    specularMaterial(0, 0, 255, 10);
-    shininess(0);
-    rotateY(-millis()/30);
-    translate(400,0);
-    sphere(40);
-    pop();
+    // push();
+    // specularMaterial(0, 0, 255, 10);
+    // //shininess(10);
+    // rotateY(-millis()/30);
+    // translate(400,0);
+    // sphere(equaRadius[6]/500);
+    // pop();
 
-    push();
-    specularMaterial(255, 50, 0, 10);
-    shininess(0);
-    rotateY(-millis()/15);
-    translate(200,0);
-    sphere(15);
-    pop();
+    // push();
+    // specularMaterial(255, 50, 0, 10);
+    // shininess(0);
+    // rotateY(-millis()/15);
+    // translate(200,0);
+    // sphere(15);
+    // pop();
+
+    createPlanet();
+
 }
+
+function createPlanet(){
+    translate(100,0);
+    for (let i = 0; i < equaRadius.length; i++) {
+        push();
+
+        emissiveMaterial(255, 255, 100);
+        //console.log(aphelion[i]);
+        translate(aphelion[i]/10000000,0);
+        sphere(equaRadius[i]/5000);
+        pop();
+        
+    }
+};
 
 // ajuste la taille du sketch en fonction de la taille de la fenêtre
 function windowResized() {
